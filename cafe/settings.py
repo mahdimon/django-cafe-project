a1cdd490e34746ab9ac8b4ea548fe8856a810c61
mahdimon <mahdimon@gmail.com>
2024-10-24 09:58:52 +0330

    database changed to server

--- cafe/settings.py
+++ cafe/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = "cafe.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.postgresql',
-# 'NAME':'cafe_dev',
-# 'USER': 'postgres',
-# 'PASSWORD': 'maktab116-1',
-# 'HOST': '85.133.205.81',
-# 'PORT': '5432'
-# }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME':'cafe_dev',
+'USER': 'postgres',
+'PASSWORD': 'maktab116-1',
+'HOST': '85.133.205.81',
+'PORT': '5432'
 }
+}
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
